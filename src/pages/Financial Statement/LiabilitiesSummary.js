edc71f737a4f7c6c3b5767d8f0c9f376cfa0ec96
youssef-ettafssaoui <yettafssaoui2@gmail.com>
2023-04-12 17:19:09 +0000

    done

--- src/pages/Financial Statement/LiabilitiesSummary.js
+++ src/pages/Financial Statement/LiabilitiesSummary.js
@@ -59,7 +59,7 @@ function LiabilitiesSummary() {
   const [year, setYear] = useState(null);
   const [liabilities, setliabilities] = useState(null);
   const [form2] = Form.useForm();
-    const [liability, setliability] = useState([]);
+  const [liability, setliability] = useState([]);
   const [editingRowbook, setEditingRowbook] = useState(null);
   const [statementcategory, setStatementCategory] = useState([{}]);
   const onChange = (key) => {
@@ -68,7 +68,6 @@ function LiabilitiesSummary() {
   // const [open, setOpen] = useState(false);
   var date;
   useEffect(() => {
-    
     console.log("year" + date);
     getLiabilities();
   }, [Company.id, year]);
@@ -79,299 +78,296 @@ function LiabilitiesSummary() {
     setYear(dateString);
   };
 
+  const getLiabilities = async () => {
+    await axios
+      .get(`${JSON_API}/Liability/summaries/${Company.id}?year=${year}`)
 
-   const getLiabilities = async () => {
-     await axios
-       .get(`${JSON_API}/Liability/summaries/${Company.id}?year=${year}`)
-
-       .then((res) => {
-         console.log(res);
-         setliabilities(res.data);
-         console.log(liabilities);
-         console.log("jkjk");
-       })
-       .catch((err) => {
-         console.log(err);
-       });
-     console.log("jkjk");
-   };
+      .then((res) => {
+        console.log(res);
+        setliabilities(res.data);
+        console.log(liabilities);
+        console.log("jkjk");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log("jkjk");
+  };
 
   const columnsBudget = [
     {
       title: <h1 style={{ textAlign: "center" }}>GL Number</h1>,
-      // dataIndex: "glAccountId",
+      dataIndex: "glNumber",
       align: "center",
-      render: (_, record) => (
-        <div style={{ textAlign: "right" }}>
-          {/* {record.chartAccount.glAccount.glNumber} */}
-        </div>
-      ),
     },
-
     {
       title: <h1 style={{ textAlign: "center" }}>Category</h1>,
-      // dataIndex: "category",
+      dataIndex: "category",
       align: "center",
-      render: (_, record) => (
-        <div style={{ textAlign: "left" }}>
-          {/* {record.financialStatementType.financialStatementCategory.label} */}
-        </div>
-      ),
     },
-
     {
       title: <h1 style={{ textAlign: "center" }}>Type</h1>,
-      // dataIndex: "note",
+      dataIndex: "type",
       align: "center",
-      render: (_, record) => (
-        <div style={{ textAlign: "left" }}>{record.note}</div>
-      ),
     },
     {
       title: <h1 style={{ textAlign: "center" }}>Year</h1>,
-     
-      // dataIndex: "year",
-      aligne: "center",
-      //   key: "year",
-      render: (text, record) => {
-        return <p style={{ textAlign: "center" }}>{record.year}</p>;
-      },
+      dataIndex: "year",
+      align: "center",
     },
     {
       title: <h1 style={{ textAlign: "center" }}>Janvier</h1>,
       dataIndex: "montant",
-      //  key: "2",
-      
-      render: (text, record) => {
-        return <p style={{ textAlign: "center" }}>{record.year}</p>;
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[0].toFixed(2)}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 0)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
+      align: "center",
     },
     {
       title: <h1 style={{ textAlign: "center" }}> Février </h1>,
-      
       dataIndex: "fevrierbudget",
-      //  key: "3",
-     
-      render: (text, record) => {
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 1)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
+      align: "center",
     },
+
     {
       title: <h1 style={{ textAlign: "center" }}>Mars</h1>,
       dataIndex: "Marsbudget",
-      key: "4",
-     
-      render: (text, record) => {
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[2]}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 2)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
+      align: "center",
     },
     {
-      title: <h1 style={{ textAlign: "center" }}>Avril</h1>,
+      title: <h1 style={{ textAlign: "center" }}> Avril </h1>,
       dataIndex: "avrilbudget",
-      key: "5",
-     
-      render: (text, record) => {
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[3]}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 3)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
+      align: "center",
     },
-    {
-      title: <h1 style={{ textAlign: "center" }}>Mai</h1>,
-    width:120,
-      key: "6",
-      
-      render: (text, record) => {
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[4]}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 4)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
-    },
-    {
-      title: <h1 style={{ textAlign: "center" }}> Juin </h1>,
-    
 
-      key: "7",
-      
-      render: (text, record) => {
-        return <p style={{ textAlign: "center" }}>{record.year}</p>;
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[5]}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 5)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}>Year</h1>,
+
+    //   // dataIndex: "year",
+    //   aligne: "center",
+    //   //   key: "year",
+    //   render: (text, record) => {
+    //     return <p style={{ textAlign: "center" }}>{record.year}</p>;
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}>Janvier</h1>,
+    //   dataIndex: "montant",
+    //   //  key: "2",
+
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}> Février </h1>,
+
+    //   dataIndex: "fevrierbudget",
+    //   //  key: "3",
+
+    //   render: (text, record) => {
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={}
+    //     //     // disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 1)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}>Mars</h1>,
+    //   dataIndex: "Marsbudget",
+    //   key: "4",
+
+    //   render: (text, record) => {
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={record.budgets[2]}
+    //     //     // disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 2)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}>Avril</h1>,
+    //   dataIndex: "avrilbudget",
+    //   key: "5",
+
+    //   render: (text, record) => {
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={record.budgets[3]}
+    //     //     // disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 3)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}>Mai</h1>,
+    //   width: 120,
+    //   key: "6",
+
+    //   render: (text, record) => {
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={record.budgets[4]}
+    //     //     // disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 4)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}> Juin </h1>,
+
+    //   key: "7",
+
+    //   render: (text, record) => {
+    //     return <p style={{ textAlign: "center" }}>{record.year}</p>;
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={record.budgets[5]}
+    //     //     // disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 5)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}>Juillet</h1>,
+    //   dataIndex: "juilletbudget",
+    //   key: "8",
+
+    //   render: (text, record) => {
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={record.budgets[6]}
+    //     //     // disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 6)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}>Août</h1>,
+    //   dataIndex: "aoutbudget",
+    //   key: "9",
+
+    //   render: (text, record) => {
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={record.budgets[7]}
+    //     //     // disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 7)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}>Septembre</h1>,
+    //   dataIndex: "septembrebudget",
+    //   key: "10",
+
+    //   render: (text, record) => {
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={record.budgets[8]}
+    //     //     // disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 8)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}>Octobre</h1>,
+    //   dataIndex: "octobrebudget",
+    //   key: "11",
+
+    //   render: (text, record) => {
+    //     return <p style={{ textAlign: "center" }}>{record.year}</p>;
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={record.budgets[9]}
+    //     //     // disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 9)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}> Novembre</h1>,
+    //   dataIndex: "novemberbudget",
+    //   key: "12",
+
+    //   render: (text, record) => {
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={record.budgets[10]}
+    //     //     // disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 10)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+    // {
+    //   title: <h1 style={{ textAlign: "center" }}>Décembre</h1>,
+    //   dataIndex: "decembrebudget",
+    //   key: "12",
+
+    //   render: (text, record) => {
+    //     // return (
+    //     //   <Input
+    //     //     type="number"
+    //     //     // value={record.budgets[11]}
+    //     //     disabled={record.confirmed}
+    //     //     // onChange={(e) => handleinputchange(e, record.id, 11)}
+    //     //     style={{ textAlign: "right" }}
+    //     //   />
+    //     // );
+    //   },
+    // },
+  ];
+
+  const items = [
+    {
+      key: "1",
+      label: <h1 style={{ width: 300, textAlign: "center" }}>Budget</h1>,
+      children: (
+        <div>
+          <Table columns={columnsBudget} dataSource={liabilities} bordered />
+        </div>
+      ),
+    },
+
+    {
+      key: "2",
+      label: <h1 style={{ width: 300, textAlign: "center" }}>Reals</h1>,
+      children: <div></div>,
     },
     {
-      title: <h1 style={{ textAlign: "center" }}>Juillet</h1>,
-      dataIndex: "juilletbudget",
-      key: "8",
-     
-      render: (text, record) => {
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[6]}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 6)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
-    },
-    {
-      title: <h1 style={{ textAlign: "center" }}>Août</h1>,
-      dataIndex: "aoutbudget",
-      key: "9",
-     
-      render: (text, record) => {
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[7]}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 7)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
-    },
-    {
-      title: <h1 style={{ textAlign: "center" }}>Septembre</h1>,
-      dataIndex: "septembrebudget",
-      key: "10",
-      
-      render: (text, record) => {
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[8]}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 8)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
-    },
-    {
-      title: <h1 style={{ textAlign: "center" }}>Octobre</h1>,
-      dataIndex: "octobrebudget",
-      key: "11",
-      
-      render: (text, record) => {
-        return <p style={{ textAlign: "center" }}>{record.year}</p>;
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[9]}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 9)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
-    },
-    {
-      title: <h1 style={{ textAlign: "center" }}> Novembre</h1>,
-      dataIndex: "novemberbudget",
-      key: "12",
-     
-      render: (text, record) => {
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[10]}
-        //     // disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 10)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
-    },
-    {
-      title: <h1 style={{ textAlign: "center" }}>Décembre</h1>,
-      dataIndex: "decembrebudget",
-      key: "12",
-      
-      render: (text, record) => {
-        // return (
-        //   <Input
-        //     type="number"
-        //     // value={record.budgets[11]}
-        //     disabled={record.confirmed}
-        //     // onChange={(e) => handleinputchange(e, record.id, 11)}
-        //     style={{ textAlign: "right" }}
-        //   />
-        // );
-      },
+      key: "3",
+      label: <h1 style={{ width: 300, textAlign: "center" }}>Perfermonce</h1>,
+      children: <div></div>,
     },
   ];
 
-   const items = [
-     {
-       key: "1",
-       label: <h1 style={{ width: 300, textAlign: "center" }}>Budget</h1>,
-       children: (
-         <div>
-           <Table columns={columnsBudget} dataSource={liabilities} bordered />
-         </div>
-       ),
-     },
-
-     {
-       key: "2",
-       label: <h1 style={{ width: 300, textAlign: "center" }}>Reals</h1>,
-       children: <div></div>,
-     },
-     {
-       key: "3",
-       label: <h1 style={{ width: 300, textAlign: "center" }}>Perfermonce</h1>,
-       children: <div></div>,
-     },
-   ];
-
- 
   return (
     <div>
       <h1 style={{ textAlign: "center" }}> Liabilities for {Company.name}</h1>
@@ -406,9 +402,7 @@ function LiabilitiesSummary() {
           </div>
         </div>
       </div>
-      <div>
-        
-      </div>
+      <div></div>
     </div>
   );
 }
